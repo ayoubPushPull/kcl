1aab4e99a01759a3a59acac500d1a30a781e0133
ayoubPushPull <contactayoubelboukhari@gmail.com>
2023-07-20 10:32:46 +0100

    commit

---
+++ app/Models/Clt_satisfait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Clt_satisfait extends Model
+{
+    use HasFactory;
+}
