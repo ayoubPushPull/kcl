6ec2c8d48e9082b50a81941ee277664eb4c95516
ayoubPushPull <contactayoubelboukhari@gmail.com>
2023-07-26 15:34:50 +0100

    COMMIT 26/07/2023

--- resources/views/general/services/services_centre_d'appel.blade.php
+++ resources/views/general/services/services_centre_d'appel.blade.php
@@ -1,4 +1,162 @@
 @extends('layouts.landingLayout')
 @section('title', "Centre D'appel")
 @section('content')
+    <div id="Content">
+        <div class="content_wrapper clearfix">
+            <div class="sections_group">
+                <div class="entry-content">
+                    <div class="section mcb-section" style="padding-top:170px; padding-bottom:40px">
+                        <div class="section_wrapper mcb-section-inner ">
+                            <div class="wrap mcb-wrap one valign-top clearfix control_height_centre">
+                                <div class="mcb-wrap-inner">
+                                    <div class="column mcb-column two-third column_column">
+                                        <div class="column_attr clearfix">
+                                            <h2>
+                                                <div class="calling_word"><span class="word_orange">CENTRE</span>
+                                                    D'APPEL
+                                                </div>
+
+                                            </h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+
+                            <div class="wrap mcb-wrap one valign-top move-up clearfix" style="margin-top:-250px"
+                                data-mobile="no-up">
+                                <div class="mcb-wrap-inner">
+                                    <div class="column mcb-column one-second column_placeholder">
+                                        <div class="placeholder">
+                                            &nbsp;
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section mcb-section" style="padding-top:0px; padding-bottom:0px">
+                        <div class="section_wrapper mcb-section-inner">
+                            <div class="wrap mcb-wrap one valign-top clearfix">
+                                <div class="mcb-wrap-inner">
+                                    <div class="column mcb-column one column_column">
+                                        <div class="column_attr clearfix">
+                                            <div class="container_grid_centre">
+                                                <div class="grid_item_centre grid_item_centre_text" style="text-align: justify">
+                                                    Notre entreprise, <strong>KADA CALLING LEADS </strong>est un acteur
+                                                    majeur dans le
+                                                    domaine des centres d'appels depuis 2014 Nous avons acquis une solide
+                                                    réputation grâce à notre engagement envers l'excellence opérationnelle
+                                                    et à notre capacité à offrir des solutions personnalisées à nos
+                                                    clients.<br>
+                                                    Notre centre d'appel est doté d'une équipe de professionnels hautement
+                                                    qualifiés et dévoués, formés pour répondre aux besoins spécifiques de
+                                                    nos clients. Nous comprenons l'importance de fournir un service
+                                                    clientèle exceptionnel pour garantir la satisfaction des utilisateurs
+                                                    finaux. C'est pourquoi nous investissons dans la formation continue de
+                                                    nos agents afin de maintenir leurs compétences à jour et de renforcer
+                                                    leur capacité à gérer diverses situations.<br>
+                                                    Nos services de centre d'appel comprennent la réception d'appels
+                                                    entrants, la gestion des appels sortants, le support technique, la
+                                                    gestion des plaintes, la prise de rendez-vous, la vente et bien
+                                                    d'autres.<br>
+                                                    Nous sommes en mesure de traiter un large éventail de demandes, que ce
+                                                    soit par téléphone, par e-mail, par chat en ligne ou par le biais des
+                                                    réseaux sociaux.<br>
+                                                    En choisissant notre centre d'appel, vous bénéficierez des avantages
+                                                    suivants :<br>
+                                                    Expertise et professionnalisme : Notre équipe possède une vaste
+                                                    expérience dans le domaine des centres d'appels et est formée pour
+                                                    représenter votre entreprise de manière professionnelle et amicale.<br>
+                                                    Flexibilité : Nous comprenons que les besoins de votre entreprise
+                                                    peuvent varier. Nous sommes prêts à nous adapter rapidement pour
+                                                    répondre à vos exigences changeantes et à fournir des solutions sur
+                                                    mesure.<br>
+                                                    Technologie avancée : Notre centre d'appel est équipé des dernières
+                                                    technologies de communication, ce qui nous permet d'offrir un service
+                                                    efficace et de qualité supérieure.<br>
+                                                    Amélioration continue : Nous sommes constamment à la recherche de moyens
+                                                    d'améliorer nos processus et nos performances. Nous évaluons
+                                                    régulièrement notre travail et sollicitons les commentaires de nos
+                                                    clients afin de garantir une amélioration continue de nos services.<br>
+                                                    Nous serions ravis de discuter plus en détail de vos besoins spécifiques
+                                                    et de la manière dont notre centre d'appel peut vous aider à atteindre
+                                                    vos objectifs. N'hésitez pas à nous contacter.
+
+                                                </div>
+                                                <div class="grid_item_centre undertext">
+                                                    <img src="{{ asset("services/kcl centre d'appel/kcl_call_center_6.jpg") }}"
+                                                        alt="kcl_call_center_6.jpg">
+                                                    <p style="text-align: justify">Nous sommes fiers de vous offrir une expérience de service à la
+                                                        clientèle de première classe grâce à nos 10 ans d'expérience dans le
+                                                        domaine. </p>
+                                                </div>
+                                                <div class="grid_item_centre undertext">
+                                                    <img src="{{ asset("services/kcl centre d'appel/kcl_call_center_5.png") }}"
+                                                        alt="kcl_call_center_5.png">
+                                                    <p style="text-align: justify">Nous sommes une équipe multilingue et nous parlons couramment le
+                                                        français, l'arabe et l'anglais, ce qui nous permet de communiquer
+                                                        efficacement avec nos clients du monde entier.</p>
+                                                </div>
+                                                <div class="grid_item_centre undertext">
+                                                    <img src="{{ asset("services/kcl centre d'appel/kcl_call_center_4.png") }}"
+                                                        alt="kcl_call_center_4.png">
+                                                    <p style="text-align: justify">Chez nous, nous mettons l'accent sur la qualité du service clientèle
+                                                        et nous nous engageons à fournir des solutions rapides et efficaces
+                                                        pour répondre à tous vos besoins. </p>
+                                                </div>
+                                                <div class="grid_item_centre undertext">
+                                                    <img src="{{ asset("services/kcl centre d'appel/kcl_call_center_3.png") }}"
+                                                        alt="kcl_call_center_3.png">
+                                                    <p style="text-align: justify">Nous comprenons que chaque client est unique et nous travaillons dur
+                                                        pour comprendre vos besoins spécifiques afin de fournir une
+                                                        expérience personnalisée et de qualité supérieure.</p>
+
+                                                </div>
+                                                <div class="grid_item_centre undertext">
+                                                    <img src="{{ asset("services/kcl centre d'appel/kcl_call_center_2.png") }}"
+                                                        alt="kcl_call_center_2.png">
+                                                    <p>Notre centre d'appel est équipé de la dernière technologie pour nous
+                                                        permettre de fournir une assistance rapide et précise à nos clients.
+                                                        Nous sommes disponibles 24 heures sur 24, 7 jours sur 7 pour
+                                                        répondre à toutes vos questions et préoccupations.</p>
+
+                                                </div>
+                                                <div class="grid_item_centre undertext">
+                                                    <img src="{{ asset("services/kcl centre d'appel/kcl_call_center_1.png") }}"
+                                                        alt="kcl_call_center_1.png">
+                                                    <p style="text-align: justify">Nous sommes fiers de fournir un service clientèle de qualité
+                                                        supérieure à tous nos clients et nous espérons avoir l'opportunité
+                                                        de travailler avec vous bientôt. N'hésitez pas à nous contacter pour
+                                                        toutes vos demandes et questions.</p>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <div class="wrap mcb-wrap one valign-top clearfix">
+                                <div class="mcb-wrap-inner">
+                                    <div class="column mcb-column one column_divider ">
+                                        <hr class="no_line" style="margin:0 auto 30px">
+                                    </div>
+                                    <div class="column mcb-column one column_button">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
