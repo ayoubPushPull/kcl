f8b93dd23103e637250c5a3170afeecc2979e91d
ayoubPushPull <contactayoubelboukhari@gmail.com>
2023-07-27 16:11:48 +0100

    push

--- resources/views/general/services/services_centre_d'appel.blade.php
+++ resources/views/general/services/services_centre_d'appel.blade.php
@@ -42,46 +42,63 @@
                                 <div class="mcb-wrap-inner">
                                     <div class="column mcb-column one column_column">
                                         <div class="column_attr clearfix">
-                                            <div class="typing-text hidden" style="text-align: justify;margin-bottom:60px">
-                                                Notre entreprise, <strong>KADA CALLING LEADS </strong>est un acteur
-                                                majeur dans le
-                                                domaine des centres d'appels depuis 2014 Nous avons acquis une solide
-                                                réputation grâce à notre engagement envers l'excellence opérationnelle
-                                                et à notre capacité à offrir des solutions personnalisées à nos
-                                                clients.<br>
-                                                Notre centre d'appel est doté d'une équipe de professionnels hautement
-                                                qualifiés et dévoués, formés pour répondre aux besoins spécifiques de
-                                                nos clients. Nous comprenons l'importance de fournir un service
-                                                clientèle exceptionnel pour garantir la satisfaction des utilisateurs
-                                                finaux. C'est pourquoi nous investissons dans la formation continue de
+                                            <div style="text-align: justify;margin-bottom:60px" @readonly(true)>
+                                                <div class="typing-text hidden">Notre entreprise, <strong>KADA CALLING LEADS
+                                                    </strong>est un acteur
+                                                    majeur dans le
+                                                    domaine des centres d'appels depuis 2014 Nous avons acquis une solide
+                                                    réputation grâce à notre engagement envers l'excellence opérationnelle
+                                                    et à notre capacité à offrir des solutions personnalisées à nos
+                                                    clients.</div>
+                                                <div class="typing-text hidden">
+                                                    Notre centre d'appel est doté d'une équipe de professionnels hautement
+                                                    qualifiés et dévoués, formés pour répondre aux besoins spécifiques de
+                                                    nos clients.</div>
+                                                <div class="typing-text hidden"> Nous comprenons l'importance de fournir un
+                                                    service
+                                                    clientèle exceptionnel pour garantir la satisfaction des utilisateurs
+                                                    finaux. </div><div class="typing-text hidden">C'est pourquoi nous investissons dans la formation
+                                                continue de
                                                 nos agents afin de maintenir leurs compétences à jour et de renforcer
-                                                leur capacité à gérer diverses situations.<br>
-                                                Nos services de centre d'appel comprennent la réception d'appels
-                                                entrants, la gestion des appels sortants, le support technique, la
-                                                gestion des plaintes, la prise de rendez-vous, la vente et bien
-                                                d'autres.<br>
-                                                Nous sommes en mesure de traiter un large éventail de demandes, que ce
-                                                soit par téléphone, par e-mail, par chat en ligne ou par le biais des
-                                                réseaux sociaux.<br>
-                                                En choisissant notre centre d'appel, vous bénéficierez des avantages
-                                                suivants :<br>
-                                                Expertise et professionnalisme : Notre équipe possède une vaste
-                                                expérience dans le domaine des centres d'appels et est formée pour
-                                                représenter votre entreprise de manière professionnelle et amicale.<br>
-                                                Flexibilité : Nous comprenons que les besoins de votre entreprise
-                                                peuvent varier. Nous sommes prêts à nous adapter rapidement pour
-                                                répondre à vos exigences changeantes et à fournir des solutions sur
-                                                mesure.<br>
-                                                Technologie avancée : Notre centre d'appel est équipé des dernières
-                                                technologies de communication, ce qui nous permet d'offrir un service
-                                                efficace et de qualité supérieure.<br>
-                                                Amélioration continue : Nous sommes constamment à la recherche de moyens
-                                                d'améliorer nos processus et nos performances. Nous évaluons
-                                                régulièrement notre travail et sollicitons les commentaires de nos
-                                                clients afin de garantir une amélioration continue de nos services.<br>
-                                                Nous serions ravis de discuter plus en détail de vos besoins spécifiques
-                                                et de la manière dont notre centre d'appel peut vous aider à atteindre
-                                                vos objectifs. N'hésitez pas à nous contacter.
+                                                leur capacité à gérer diverses situations. </div><div class="typing-text hidden">
+                                                    Nos services de centre d'appel comprennent la réception d'appels
+                                                    entrants, la gestion des appels sortants, le support technique, la
+                                                    gestion des plaintes, la prise de rendez-vous, la vente et bien
+                                                    d'autres.</div>
+                                                <div class="typing-text hidden">
+                                                    Nous sommes en mesure de traiter un large éventail de demandes, que ce
+                                                    soit par téléphone, par e-mail, par chat en ligne ou par le biais des
+                                                    réseaux sociaux.</div>
+                                                <div class="typing-text hidden">
+                                                    En choisissant notre centre d'appel, vous bénéficierez des avantages
+                                                    suivants :</div>
+                                                <div class="typing-text hidden">
+                                                    Expertise et professionnalisme : Notre équipe possède une vaste
+                                                    expérience dans le domaine des centres d'appels et est formée pour
+                                                    représenter votre entreprise de manière professionnelle et amicale.
+                                                </div>
+                                                <div class="typing-text hidden">
+                                                    Flexibilité : Nous comprenons que les besoins de votre entreprise
+                                                    peuvent varier. </div>
+                                                <div class="typing-text hidden">Nous sommes prêts à nous adapter rapidement
+                                                    pour
+                                                    répondre à vos exigences changeantes et à fournir des solutions sur
+                                                    mesure.</div>
+                                                <div class="typing-text hidden">
+                                                    Technologie avancée : Notre centre d'appel est équipé des dernières
+                                                    technologies de communication, ce qui nous permet d'offrir un service
+                                                    efficace et de qualité supérieure.</div>
+                                                <div class="typing-text hidden">
+                                                    Amélioration continue : Nous sommes constamment à la recherche de moyens
+                                                    d'améliorer nos processus et nos performances. Nous évaluons
+                                                    régulièrement notre travail et sollicitons les commentaires de nos
+                                                    clients afin de garantir une amélioration continue de nos services.
+                                                </div>
+                                                <div class="typing-text hidden">
+                                                    Nous serions ravis de discuter plus en détail de vos besoins spécifiques
+                                                    et de la manière dont notre centre d'appel peut vous aider à atteindre
+                                                    vos objectifs.</div>
+                                                <div class="typing-text hidden"> N'hésitez pas à nous contacter.</div>
                                             </div>
                                             <hr>
                                             <div class="container_grid_centre">
@@ -121,7 +138,8 @@
                                                 <div class="grid_item_centre undertext" tabindex="0">
                                                     <img src="{{ asset("services/kcl centre d'appel/kcl_call_center_2.png") }}"
                                                         alt="kcl_call_center_2.png">
-                                                    <p>Notre centre d'appel est équipé de la dernière technologie pour nous
+                                                    <p style="text-align: justify">Notre centre d'appel est équipé de la
+                                                        dernière technologie pour nous
                                                         permettre de fournir une assistance rapide et précise à nos clients.
                                                         Nous sommes disponibles 24 heures sur 24, 7 jours sur 7 pour
                                                         répondre à toutes vos questions et préoccupations.</p>
@@ -156,10 +174,6 @@
                             </div>
                         </div>
                     </div>
-
-
-
-
                 </div>
             </div>
         </div>
